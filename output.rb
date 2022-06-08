2a7882cda73a13631238e687231097d92608b0d7
Ahmet Hakan Aktuğ <ahmethakanaktug@gmail.com>
2022-06-09 00:13:07 +0300

    Output and variables

---
+++ output.rb
@@ -0,0 +1,29 @@
+# print writes to std output
+print "Hello, world!"
+
+# puts writes to std output and adds a newline
+puts "Hello, world!"
+
+# multiline coments are bit weird
+
+=begin
+Hello, I'm Ahmet
+Writing
+multline
+comments
+=end
+
+# Variables are not statically typed
+name = "Hakan"
+age = 20
+gpa = 3.41
+
+puts name
+puts age
+puts gpa
+
+puts #additional line between outputs
+
+# Variables can be reassigned to different types
+name = 15
+puts name
