ba0762149a107fe3971255576a579d52d4584c4c
Ahmet Hakan Aktuğ <ahmethakanaktug@gmail.com>
2022-06-09 00:53:21 +0300

    Math

---
+++ 04-math.rb
@@ -0,0 +1,24 @@
+a = 5**3 # Power fucntion
+puts a
+
+
+a = 10 % 6 # mod
+puts a
+
+
+# Ceil, floor, round
+a = 21.23
+puts a.ceil
+puts a.floor
+puts a.round
+
+
+# Math library includes various operations
+puts Math.sqrt(16)
+puts Math.log(2.71828)
+# trig. funcitons are also included
+
+
+# The result of operations are dependant on operand types
+puts 14/3 # returns 4 - only the integer part (nor rounding)
+puts 14/3.0 # real division
