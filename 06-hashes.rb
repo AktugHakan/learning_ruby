ce87927e9719a9a3b6778b8fc18392aec72ba2fb
Ahmet Hakan Aktuğ <ahmethakanaktug@gmail.com>
2022-06-13 00:28:42 +0300

    Hash

---
+++ 06-hashes.rb
@@ -0,0 +1,16 @@
+# Store key-value pairs efficiently
+
+# Defining a hash
+sehirler = {
+    "Çankırı" => 18,
+    "İstanbul" => 34,
+    "K.Maraş" => 46,
+    "Ş.Urfa" => 63,
+    "Kırıkkale" => 71,
+    "Adana" => 01,
+    "Ankara" => 06
+}
+# Keys are unique
+
+# Values are accessed using keys
+puts sehirler["K.Maraş"]
