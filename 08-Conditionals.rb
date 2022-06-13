b76f3ff0cb469fa33f50f9971dffc102530db6eb
Ahmet Hakan Aktuğ <ahmethakanaktug@gmail.com>
2022-06-13 23:30:46 +0300

    Conditionals

---
+++ 08-Conditionals.rb
@@ -0,0 +1,30 @@
+print "Write an integer: "
+small = gets.chomp.to_i
+
+print "Write a LARGER integer: "
+large = gets.chomp.to_i
+
+if large > small
+    puts "Fantastic job done here. Congrats!"
+elsif large == small # "elsif"
+    puts "They are equal. Try again!"
+else
+    puts "Nope"
+end
+
+
+# Switch-Case structure
+puts "What to do today?\na - Say hi\nb - Attack\nc - Flee"
+choice = gets.chomp
+case choice
+when "a", "A"
+    puts "Hi!"
+when "b", "B"
+    puts "Attacked!"
+when "c", "C"
+    puts "Run!"
+else
+    puts "Unvalid"
+end
+
+
