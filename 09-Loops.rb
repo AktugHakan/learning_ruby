aaa329cd8d5b90da5922acebf2c2760c4ab5aa48
Ahmet Hakan Aktuğ <ahmethakanaktug@gmail.com>
2022-06-20 13:50:12 +0300

    Loops

--- 09-Loops.rb
+++ 09-Loops.rb
@@ -27,3 +27,39 @@ Using .. for range, includes the beginning and end |                        begi
 Using ... for range, includes the beggining but does not include the end |  begin <= x < end 
 =end
 
+
+
+# We can iterate over lists using each method
+def print_iterable(iterable)
+    # Check if the parameter is iterable
+    if iterable.respond_to? "each"
+        print "["
+        # each method calls the given lambda function (do ... end) for each element in the list
+        iterable.each do |number| # parameters of lambda functions is between long lines
+            print number.to_s + ", "
+        end
+        print "]"
+        puts
+    else
+        raise TypeError "Given item is not iterable"
+    end
+end
+puts "---Printing the list---"
+print_iterable(my_list)
+
+
+# we can write a better version of our code using methods
+def print_iterable2(iterable)
+    # Check if the parameter is iterable
+    if iterable.respond_to? "join"
+        print "["
+        # join method converts array to a string, seperated with given seperator
+        print iterable.join(" | ")
+        print "]"
+        puts
+    else
+        raise TypeError "Given item is not iterable"
+    end
+end
+print_iterable2(my_list)
+print_iterable2(loop_counter) # non-iterable type will raise error
